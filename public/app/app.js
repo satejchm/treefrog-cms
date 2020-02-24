71dad78442af512fbb1b3b8d44d593253356a943
Sarah Tejchma <satejchm@iu.edu>
2020-02-24 12:37:32 -0500

    changes for tuesday hw 4

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,26 @@
 function initButtons() {
+  addGetStartedListener();
+  createMainNav();
+  createSubNav();
+  createHomeContent();
+  validateInput();
+}
+
+function createHomeContent() {
+  //when you clock on home it will take you back to home page
+  $("#home").click(function(e) {
+    $("#addNav div").removeClass("active");
+    $("#home div").addClass("active");
+
+    $(".text-wrapper").html(TREEFROG_SERVICE.getHomeContent());
+    $(".btn-holder").html(TREEFROG_SERVICE.getHomeStartButton());
+    $(".main-nav").off();
+    $(".sub-nav").off();
+    initButtons();
+  });
+}
+
+function addGetStartedListener() {
   //when you click get started it leads you to add nav page
   $(".get-started").click(function(e) {
     $("#home div").removeClass("active");
@@ -6,40 +28,78 @@ function initButtons() {
 
     $(".text-wrapper").html(TREEFROG_SERVICE.getGetStartedContent());
     $(".btn-holder").html(TREEFROG_SERVICE.getCreateNavButtons());
-
-    //when you click on create main nav it opens modal
-    $(".main-nav").click(function(e) {
-      $(".modal").css("display", "flex");
-      $(".modal").css("color", "white");
-      $(".alert-box").html(TREEFROG_SERVICE.getModalContent());
-      //$(".cancel").addClass("btn btn-light");
-      $(".cancel").click(function(e) {
-        $(".modal").css("display", "none");
-      });
+    createMainNav();
+    createSubNav();
+    $(".get-started").off();
+  });
+}
+function createMainNav() {
+  //when you click on create main nav it opens modal
+  $(".main-nav").click(function(e) {
+    $(".modal").css("display", "flex");
+    $(".alert-box").html(TREEFROG_SERVICE.getModalContent());
+    //when you click on create main nav button it checks for empty or if there is a value, returns it
+    $(".create-main-nav").click(function(e) {
+      validateInput();
     });
-
-    //when you click on create sub nav it opens a modal
-    $(".sub-nav").click(function(e) {
-      $(".modal").css("display", "flex");
-
-      $(".modal").css("color", "white");
-      $(".alert-box").html(TREEFROG_SERVICE.getModalContentSub());
-      $(".cancel").click(function(e) {
-        $(".modal").css("display", "none");
-      });
-    });
-
-    //when you clock on home it will take you back to home page
-    $("#home").click(function(e) {
-      $("#addNav div").removeClass("active");
-      $("#home div").addClass("active");
-
-      $(".text-wrapper").html(TREEFROG_SERVICE.getHomeContent());
-      $(".btn-holder").html(TREEFROG_SERVICE.getHomeStartButton());
+    //close modal when you click on cancel or x button
+    $(".closeModal").click(function(e) {
+      $(".modal").css("display", "none");
     });
   });
 }
 
+function createSubNav() {
+  //when you click on create sub nav it opens a modal
+  $(".sub-nav").click(function(e) {
+    $(".modal").css("display", "flex");
+    $(".alert-box").html(TREEFROG_SERVICE.getModalContentSub());
+    //when you click on create sub nav button it takes you to new page
+    $(".create-sub-nav").click(function(e) {
+      $(".text-wrapper").html(TREEFROG_SERVICE.getMainNavCreateContent());
+      $(".modal").css("display", "none");
+      $(".btn-holder").css("display", "none");
+    });
+    //close modal when you click on cancel or x button
+    $(".closeModal").click(function(e) {
+      $(".modal").css("display", "none");
+    });
+  });
+}
+
+function validateInput() {
+  //if input box is empty or has no value return an error alert
+  if ($(".inputBox").val() === "" || $("#inputBox").val() === null) {
+    swal("Please enter navigation name.");
+    return false;
+  }
+  if ($(".inputBox").val() === "home" || $("#inputBox").val() === "about") {
+    swal(
+      "You have already created a navigation with this name. Please create a new navigation name."
+    );
+    return false;
+  }
+  if ($(".inputBox").val()) {
+    //if someone entered in the input, return the input value
+    swal(
+      "You added " +
+        $(".inputBox")
+          .val()
+          .toLowerCase() +
+        " to your navigation!"
+    );
+    console.log(
+      $(".inputBox")
+        .val()
+        .toLowerCase()
+    );
+    $(".modal").css("display", "none");
+    $(".text-wrapper").html(TREEFROG_SERVICE.getMainNavCreateContent());
+    $(".btn-holder").html(TREEFROG_SERVICE.getSavePageInfoButton());
+  }
+}
+
 $(document).ready(function() {
   initButtons();
+  addGetStartedListener();
 });
--- public/app/service.js
+++ public/app/service.js
@@ -25,7 +25,8 @@ var TREEFROG_SERVICE = (function() {
     <p>
       Your fist step is to click on the Add Navigation link and add your
       first navigation link.
-    </p>`;
+    </p></div>
+    `;
 
     return homeContent;
   };
@@ -37,51 +38,48 @@ var TREEFROG_SERVICE = (function() {
   };
 
   var _getModalContent = function() {
-    let modalContent = `<div style="margin-top: 20px; margin: 15px; display: flex; flex-direction: column; align-items: center;"><h2>Use this box to create navigation links.</h2>
-    <p >
+    let modalContent = `<h2>Use this box to create navigation links.</h2>
+    <p style="margin: 0 15px 50px 15px;">
       You can create main navigation and sub navigation. To create sub-navigation you will need to first select a main nav and then create the sub-nav.
     </p>
-    <p style="margin-bottom: 10px;">
+    <p>
       Using the text box below enter the name of your main navigation.
     </p>
-    <input style="padding: 5px;
-    width: 200px;
-    height: 30px;
-    font-size: 20px; border-radius: 5px; " class="inputBox" type="text" name="navname" placeholder="products"><br>
-    </div>
-    <div style="display: flex; flex-direction: row; align-items: center;  justify-content: center;">
-    <span style="justify-content: center; margin-right: 30px;
-    background-color: #f2dcc9;
-    color: #393c40;"class="btn btn-light">Create Main Nav</span><br><span style=" margin-right: 30px;
-    background-color: #f2dcc9;
-    color: #393c40;" class="btn btn-light cancel">Cancel</span></div>`;
+    <form id="form">
+    <input name="inputBox" class="inputBox" style="margin-bottom: 30px;" value="" type="text" placeholder="products"><br>
+    <div style="flex-direction: row; margin-top: 30px;">
+    <span class="btn btn-light create-main-nav">Create Main Nav</span></form><span class="btn btn-light closeModal">Cancel</span></div>`;
 
     return modalContent;
   };
 
   var _getModalContentSub = function() {
-    let modalContent = `<div style="margin: 20px; display: flex; flex-direction: column; align-items: center;"><h2>Create Sub Navigation</h2>
+    let modalContent = `<h2>Create Sub Navigation</h2>
     <p>In order to create a sub navigation you need to select a main nav.</p>
     <p>
       Select a main navigation
     </p>
-    <select style="width: 175px; font-size: 20px; height: 30px; margin-bottom: 15px; border-radius: 5px;" id="list"><option value="Products">Products</option><option value="Other">About</option><option value="Other">Contact</option></select>
-    <p style="margin-bottom: 20px;">
+    <select class="inputBox" id="list"><option value="Products">Products</option><option value="Other">About</option><option value="Other">Contact</option></select>
+    <p>
       Using the text box below enter the name of your sub navigation.
     </p>
-    <input style="padding: 5px;
-    width: 200px;
-    height: 30px;
-    font-size: 20px; border-radius: 5px;" class="inputBox" type="text" name="navname" placeholder="Bikes">
-    </div>
-    <div style="display: flex; flex-direction: row; align-items: center;  justify-content: center;">
-    <span style="margin: 10px;
-    background-color: #f2dcc9;
-    color: #393c40;" class="btn btn-light">Create Sub Nav</span><span style="margin-right: 30px;
-    background-color: #f2dcc9;
-    color: #393c40;" class="btn btn-light cancel">Cancel</span></div>`;
+    <input class="inputBox" type="text" name="navname" placeholder="Bikes">
+    <div style="margin-top: 40px;">
+    <span class="btn btn-light create-sub-nav">Create Sub Nav</span><span class="btn btn-light closeModal">Cancel</span></div>`;
 
     return modalContent;
+  };
+
+  var _getMainNavCreateContent = function() {
+    let contentStr = `<h1>Treefrog CMS</h1><p>Now you have your navigation set now you can create your content. Below you will see your navigation name and a text editor. Create your content in the text editor and then click on "Save Page Info". Once you have done that click on "PREVIEW SITE" to see what your web page looks like.</p>`;
+
+    return contentStr;
+  };
+
+  var _getSavePageInfoButton = function() {
+    let savePage = `<span class="btn btn-dark sub-page">Save Page Info</span>`;
+
+    return savePage;
   };
 
   return {
@@ -90,6 +88,8 @@ var TREEFROG_SERVICE = (function() {
     getHomeContent: _getHomeContent,
     getHomeStartButton: _getHomeStartButton,
     getModalContent: _getModalContent,
-    getModalContentSub: _getModalContentSub
+    getModalContentSub: _getModalContentSub,
+    getMainNavCreateContent: _getMainNavCreateContent,
+    getSavePageInfoButton: _getSavePageInfoButton
   };
 })();
